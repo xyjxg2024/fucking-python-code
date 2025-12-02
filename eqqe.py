32c42b0b439f9291248de8d7c748ee0954c5bacf
xyjxg2024 <zzy1179804909@163.com>
2025-12-02 21:06:35 +0800

    next

--- eqqe.py
+++ eqqe.py
@@ -1,6 +1,23 @@
-# a,b=map(int,input().split())
-# list_a=list(map(int,input().split()))
-list_a=[128129,128130,128131,128132]
-for i in list_a:
-    print(chr(i))
-# print(chr(128129))
+a=int(input())
+for i in range(a):
+    list_a=[]
+    x,y=map(int,input().split())
+    num_a=2**(x+1)
+    num_b=num_a-y
+    ans=0
+    while num_a==num_b:
+        ans+=1
+        if num_a>num_b:
+            if num_a//2==0:
+                num_b+=num_a//2
+                num_a=num_a//2
+            else:
+                num_a+=num_b//2
+                num_b=num_b//2
+        else:
+            if num_b//2==0:
+                num_a+=num_b//2
+                num_b=num_b//2
+            else:
+                num_b+=num_a//2
+                num_a=num_a//2
